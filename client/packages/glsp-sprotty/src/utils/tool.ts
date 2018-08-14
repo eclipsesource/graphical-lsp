87d8b7bfb39c572d86535f4c1c4ca376c6faa384
Tobias Ortmayr <tortmayr@eclipsesource.com>
2018-08-14 19:55:07 +0200

    [Client] Add sprotty feature for tools

---
+++ client/packages/glsp-sprotty/src/utils/tool.ts
@@ -0,0 +1,17 @@
+export interface Tool {
+    /**
+     * The tool identifier.
+     */
+    readonly id: string;
+
+    /**
+     * The tool name.
+     */
+    readonly name: string;
+
+    /**
+     * The tool type.
+     */
+    readonly toolType: string;
+
+}
