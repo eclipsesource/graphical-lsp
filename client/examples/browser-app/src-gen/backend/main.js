87bd2239eec9fdd0cb65f0e97bb14a4e6f11c260
Tobias Ortmayr <tortmayr@eclipsesource.com>
2018-08-20 19:39:48 +0200

    [Client] Remove tracked gen files

--- client/examples/browser-app/src-gen/backend/main.js
+++
@@ -1,9 +0,0 @@
-// @ts-check
-const serverPath = require('path').resolve(__dirname, 'server');
-const address = require('@theia/core/lib/node/cluster/main').default(serverPath);
-address.then(function (address) {
-    if (process && process.send) {
-        process.send(address.port.toString());
-    }
-});
-module.exports = address;
