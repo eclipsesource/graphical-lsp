04d328bb920b4f9755af71e0436efd56cc3499ae
Tobias Ortmayr <tortmayr@eclipsesource.com>
2018-08-21 16:55:12 +0200

    [Client] Add missing copyright headers

--- client/examples/workflow-sprotty/src/model-factory.ts
+++ client/examples/workflow-sprotty/src/model-factory.ts
@@ -1,3 +1,13 @@
+/*******************************************************************************
+ * Copyright (c) 2018 Tobias Ortmayr.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *   
+ * Contributors:
+ * 	Tobias Ortmayr - initial API and implementation
+ ******************************************************************************/
 import { SGraphFactory, SModelElementSchema, SParentElement, SChildElement, getBasicType, getSubType } from "sprotty/lib";
 import { WeightedEdge, TaskNode, ActivityNode } from "./model";
 import { TaskNodeSchema, ActivityNodeSchema, WeightedEdgeSchema } from "./model-schema";
