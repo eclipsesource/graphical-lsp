86154bbe966f3301cc718f4e2755ae4bfeedd2f7
Philip Langer <planger@eclipsesource.com>
2018-08-28 23:27:08 +0200

    Append operation kind to make action id unique and remove generic id

--- server/glsp-api/src/main/java/at/tortmayr/glsp/api/action/kind/ExecuteOperationAction.java
+++ server/glsp-api/src/main/java/at/tortmayr/glsp/api/action/kind/ExecuteOperationAction.java
@@ -11,21 +11,14 @@
 package at.tortmayr.glsp.api.action.kind;
 
 import at.tortmayr.glsp.api.action.Action;
-import at.tortmayr.glsp.api.operations.OperationKind;
 
-public class ExecuteOperationAction extends Action {
+public abstract class ExecuteOperationAction extends Action {
 	
 	private String operationKind;
-	private String operationId;
-	
-	public ExecuteOperationAction() {
-		super(ActionKind.EXECUTE_OPERATION);
-		this.operationKind = OperationKind.GENERIC;
-	}
 
-	public ExecuteOperationAction(String operationId) {
-		this();
-		this.operationId = operationId;
+	public ExecuteOperationAction(String operationKind) {
+		super(ActionKind.EXECUTE_OPERATION + "_" + operationKind);
+		this.operationKind = operationKind;
 	}
 
 	public String getOperationKind() {
@@ -34,45 +27,6 @@ public class ExecuteOperationAction extends Action {
 
 	public void setOperationKind(String operationKind) {
 		this.operationKind = operationKind;
-	}
-
-	public String getOperationId() {
-		return operationId;
-	}
-
-	public void setOperationId(String operationId) {
-		this.operationId = operationId;
-	}
-
-	@Override
-	public int hashCode() {
-		final int prime = 31;
-		int result = super.hashCode();
-		result = prime * result + ((operationId == null) ? 0 : operationId.hashCode());
-		result = prime * result + ((operationKind == null) ? 0 : operationKind.hashCode());
-		return result;
-	}
-
-	@Override
-	public boolean equals(Object obj) {
-		if (this == obj)
-			return true;
-		if (!super.equals(obj))
-			return false;
-		if (getClass() != obj.getClass())
-			return false;
-		ExecuteOperationAction other = (ExecuteOperationAction) obj;
-		if (operationId == null) {
-			if (other.operationId != null)
-				return false;
-		} else if (!operationId.equals(other.operationId))
-			return false;
-		if (operationKind == null) {
-			if (other.operationKind != null)
-				return false;
-		} else if (!operationKind.equals(other.operationKind))
-			return false;
-		return true;
 	}
 
 }
