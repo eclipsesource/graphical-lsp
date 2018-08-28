2584913b024a4a9134969f3a872e2c9658cf03cc
Tobias Ortmayr <tortmayr@eclipsesource.com>
2018-08-28 19:50:28 +0200

    Add save command to diagram menu

--- client/packages/glsp-theia-extension/src/browser/diagram/glsp-diagram-commands.ts
+++ client/packages/glsp-theia-extension/src/browser/diagram/glsp-diagram-commands.ts
@@ -1,21 +1,26 @@
 import { injectable, inject } from "inversify";
 import { MenuContribution, MenuPath, MAIN_MENU_BAR, MenuModelRegistry, CommandContribution, CommandRegistry } from "@theia/core";
 import { DiagramMenus, DiagramCommandHandler } from "theia-glsp/lib";
-import { ApplicationShell } from "@theia/core/lib/browser";
-import { ExecuteNodeCreationToolAction } from "glsp-sprotty/lib";
+import { ApplicationShell, Widget } from "@theia/core/lib/browser";
+import { ExecuteNodeCreationToolAction, SaveModelAction } from "glsp-sprotty/lib";
 
 
 
 export namespace GLSPDiagramCommands {
-    export const CREATE_NODE = 'glsp:diagra:createNode'
+    export const CREATE_NODE = 'glsp:diagram:createNode';
+    export const SAVE_MODEL = 'glsp:diagram:saveModel'
 }
 @injectable()
 export class GLSPDiagramMenuContribution implements MenuContribution {
     registerMenus(menus: MenuModelRegistry): void {
         menus.registerMenuAction(DiagramMenus.DIAGRAM, {
             commandId: GLSPDiagramCommands.CREATE_NODE
+        });
+        menus.registerMenuAction(DiagramMenus.DIAGRAM, {
+            commandId: GLSPDiagramCommands.SAVE_MODEL
         })
     }
+
 }
 
 @injectable()
@@ -28,16 +33,23 @@ export class GLSPDiagramCommandContribution implements CommandContribution {
         registry.registerCommand({
             id: GLSPDiagramCommands.CREATE_NODE,
             label: 'Create Node'
-        }),
-            registry.registerHandler(
-                GLSPDiagramCommands.CREATE_NODE,
-                new DiagramCommandHandler(this.shell, widget => {
-                    let toolId = "wf-automated-task-tool"
-                    let point = { x: 50, y: 50 }
-                    let containerId = undefined
-                    let action = new ExecuteNodeCreationToolAction(toolId, point, containerId);
-                    widget.actionDispatcher.dispatch(action)
-                }))
+        });
+        registry.registerCommand({
+            id: GLSPDiagramCommands.SAVE_MODEL,
+            label: 'Save Model'
+        })
+        registry.registerHandler(
+            GLSPDiagramCommands.CREATE_NODE,
+            new DiagramCommandHandler(this.shell, widget => {
+                let toolId = "wf-automated-task-tool"
+                let point = { x: 50, y: 50 }
+                let containerId = undefined
+                let action = new ExecuteNodeCreationToolAction(toolId, point, containerId);
+                widget.actionDispatcher.dispatch(action)
+            }))
+        registry.registerHandler(GLSPDiagramCommands.SAVE_MODEL, new DiagramCommandHandler(this.shell, widget => {
+            widget.actionDispatcher.dispatch(new SaveModelAction())
+        }))
     }
 
 
