5fba4bbe35be2a233a6618b29cc86cb7c43ac507
Tobias Ortmayr <tortmayr@eclipsesource.com>
2018-08-29 17:55:12 +0200

    Fix merge conflicts

--- client/packages/glsp-sprotty/src/index.ts
+++ client/packages/glsp-sprotty/src/index.ts
@@ -12,11 +12,10 @@ export * from './features/operation/set-operations'
 export * from './utils/operation'
 import saveModule from './features/save/di.config'
 import paletteModule from './features/palette/di.config'
-<<<<<<< Upstream, based on origin/refactor-model-operations
+
 import moveToolModule from './features/tool/di.config'
 
 export { saveModule, paletteModule, moveToolModule }
-=======
+
 export * from './features/tool/execute-tool'
-export { saveModule, paletteModule }
->>>>>>> 0d058d0 Add prototypical palette implementation on server side
+
