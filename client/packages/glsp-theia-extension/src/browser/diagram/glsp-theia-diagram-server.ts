41360842e1b7cb7703b8496bb8536e5b7de2dcaa
Tobias Ortmayr <tortmayr@eclipsesource.com>
2018-08-29 17:55:12 +0200

    Fix merge conflicts

--- client/packages/glsp-theia-extension/src/browser/diagram/glsp-theia-diagram-server.ts
+++ client/packages/glsp-theia-extension/src/browser/diagram/glsp-theia-diagram-server.ts
@@ -10,12 +10,8 @@
  ******************************************************************************/
 import { injectable, inject } from "inversify";
 import { TheiaDiagramServer } from "theia-glsp/lib";
-import { ActionMessage, Tool, ActionHandlerRegistry, SetToolsCommand, RequestToolsAction, SaveModelAction, SetToolsAction, TYPES, IActionDispatcher, ViewerOptions, SModelStorage, ILogger, ToolType, Action, ExecuteNodeCreationToolAction, ExecuteToolAction, alignFeature, CreateConnectionAction, MoveAction, MoveCommand } from "glsp-sprotty/lib";
-import { MenuModelRegistry, CommandRegistry, SelectionService, Command } from "@theia/core/lib/common";
-import { UriCommandHandler, UriAwareCommandHandler } from "@theia/core/lib/common/uri-command-handler";
-import URI from "@theia/core/lib/common/uri";
-import { EDITOR_CONTEXT_MENU } from "@theia/editor/lib/browser";
-import { ActionMessage, ActionHandlerRegistry, SaveModelAction, TYPES, IActionDispatcher, ViewerOptions, SModelStorage, ILogger, Action, ExecuteNodeCreationToolAction, ExecuteToolAction, alignFeature, CreateConnectionAction, SetOperationsCommand, RequestOperationsAction } from "glsp-sprotty/lib";
+import { ActionMessage, ActionHandlerRegistry, SaveModelAction, TYPES, IActionDispatcher, ViewerOptions, SModelStorage, ILogger, Action, ExecuteNodeCreationToolAction, ExecuteToolAction, alignFeature, CreateConnectionAction, MoveAction, MoveCommand, SetOperationsCommand, RequestOperationsAction } from "glsp-sprotty/lib";
+
 
 
 
