b11555d766b57c4518ef434d96491df0ef24d0af
Tobias Ortmayr <tortmayr@eclipsesource.com>
2018-10-10 10:35:56 +0200

    Add travis deployment configuration

---
+++ scripts/deployMaven.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+if 	git diff --name-only HEAD^| grep -q "^server" 
+
+then 
+	echo "Deploy maven snapshot plugins"
+	cd ../server
+	mvn deploy --settings ./settings.xml -Dmaven.test.skip=true
+else	echo "No maven pacakges have been changed. Skip deployment"
+fi
+
---
+++ scripts/deployNPM.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+if 	git diff --name-only HEAD^| grep -q "^client" 
+
+then 
+	echo "Deploy next-packages to npm"
+	cd ../client
+	yarn run publish:next
+else	echo "No NPM packages have been changed. Skip deployment"
+fi
+
