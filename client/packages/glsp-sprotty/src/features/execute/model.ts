a9d4b67d813f4ebce68e7f07d2294bed4fa7f9db
Tobias Ortmayr <tortmayr@eclipsesource.com>
2018-10-11 11:19:55 +0200

    Feature for executing commands on server side

---
+++ client/packages/glsp-sprotty/src/features/execute/model.ts
@@ -0,0 +1,21 @@
+/*******************************************************************************
+ * Copyright (c) 2018 EclipseSource Services GmbH.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v2.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v20.html
+ *   
+ * Contributors:
+ * 	Tobias Ortmayr - initial API and implementation
+ ******************************************************************************/
+import { SModelExtension, SModelElement } from "sprotty/lib";
+
+export const executeCommandFeature = Symbol('executeFeature');
+
+export interface CommandExecutor extends SModelExtension {
+    commandId: string
+}
+
+export function isCommandExecutor(element: SModelElement): element is SModelElement & CommandExecutor {
+    return element.hasFeature(executeCommandFeature)
+}
