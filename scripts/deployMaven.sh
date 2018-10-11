f00f2389bce12a297e33fbbcf344d198d49c7415
Tobias Ortmayr <tortmayr@eclipsesource.com>
2018-10-11 13:40:00 +0200

    Update travis configuration

--- scripts/deployMaven.sh
+++ scripts/deployMaven.sh
@@ -5,7 +5,7 @@ if 	git diff --name-only HEAD^| grep -q "^server"
 then 
 	echo "Deploy maven snapshot plugins"
 	cd ../server
-	mvn deploy --settings ./settings.xml -Dmaven.test.skip=true
+	mvn deploy --settings ./settings.xml -Dmaven.test.skip=true -U -Prelease
 else	echo "No maven pacakges have been changed. Skip deployment"
 fi
 
