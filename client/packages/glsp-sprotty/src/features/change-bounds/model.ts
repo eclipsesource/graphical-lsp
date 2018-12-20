6250e4a9aab333d72caf71bde5ee3324f152f44e
Martin Fleck <mfleck@eclipsesource.com>
2018-12-20 16:24:34 +0100

    #46: Unify move tool and resize tool to change-bounds tool

---
+++ client/packages/glsp-sprotty/src/features/change-bounds/model.ts
@@ -0,0 +1,34 @@
+/*******************************************************************************
+ * Copyright (c) 2018 EclipseSource Services GmbH and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v2.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v20.html
+ *
+ * Contributors:
+ *  Martin Fleck - initial API and implementation
+ ******************************************************************************/
+import { BoundsAware, Hoverable, hoverFeedbackFeature, isBoundsAware, isMoveable, Locateable, SChildElement, SModelElement } from "sprotty/lib";
+import { ResizeHandleLocation } from "../resize/model";
+
+
+export function isBoundsAwareMoveable(element: SModelElement): element is SModelElement & Locateable & BoundsAware {
+    return isMoveable(element) && isBoundsAware(element);
+}
+
+export class SResizeHandle extends SChildElement implements Hoverable {
+    type: string = 'resize-handle';
+    hoverFeedback: boolean = false;
+    location: ResizeHandleLocation;
+
+    constructor(location?: ResizeHandleLocation) {
+        super();
+        if (location) {
+            this.location = location;
+        }
+    }
+
+    hasFeature(feature: symbol): boolean {
+        return feature === hoverFeedbackFeature;
+    }
+}
---
+++ client/packages/glsp-sprotty/src/features/tools/change-bounds-tool.ts
@@ -0,0 +1,269 @@
+/*******************************************************************************
+ * Copyright (c) 2018 EclipseSource Services GmbH and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v2.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v20.html
+ *
+ * Contributors:
+ *  Camille Letavernier - initial API and implementation
+ *  Philip Langer - migration to tool manager API
+ *  Martin Fleck - migration to use of change bounds action
+ ******************************************************************************/
+import { inject, injectable } from "inversify";
+// tslint:disable-next-line:max-line-length
+import { Action, Bounds, BoundsAware, ElementAndBounds, findParentByFeature, isViewport, MouseListener, MouseTool, Point, SetBoundsAction, SModelElement, SModelRoot, SParentElement } from "sprotty/lib";
+import { CommandStackObserver, ObservableCommandStack } from "../../base/command-stack";
+import { forEachElement, getIndex, isSelectedBoundsAware } from "../../utils/smodel-util";
+import { isBoundsAwareMoveable } from "../change-bounds/model";
+import { ChangeBoundsOperationAction } from "../operation/operation-actions";
+import { isResizeable, ResizeHandleLocation, SResizeHandle } from "../resize/model";
+import { addResizeHandles, SwitchResizeModeAction } from "../resize/resize";
+import { FeedbackMoveMouseListener } from "../tool-feedback/move-tool-feedback";
+import { Tool } from "../tool-manager/tool";
+
+/**
+ * The change bounds tool has the license to move multiple elements or resize a single element by implementing the ChangeBounds operation.
+ * In contrast to Sprotty's implementation this tool only sends a `ChangeBoundsOperationAction` when an operation has finished and does not
+ * provide client-side live updates to improve performance.
+ *
+ * | Operation | Client Update    | Server Update
+ * +-----------+------------------+----------------------------
+ * | Move      | MoveAction       | ChangeBoundsOperationAction
+ * | Resize    | SetBoundsAction  | ChangeBoundsOperationAction
+ *
+ * To provide a visual client updates during move we install the `FeedbackMoveMouseListener` and to provide visual client updates during resize
+ * and send the server updates we install the `ChangeBoundsMouseListener`.
+ */
+@injectable()
+export class ChangeBoundsTool implements Tool {
+
+    static ID = "glsp.change-bounds-tool";
+    readonly id = ChangeBoundsTool.ID;
+
+    protected feedbackMoveMouseListener: FeedbackMoveMouseListener;
+    protected changeBoundsMouseListener: ChangeBoundsMouseListener;
+
+    constructor(@inject(MouseTool) protected mouseTool: MouseTool,
+        @inject(ObservableCommandStack) protected commandStack: ObservableCommandStack) { }
+
+    enable() {
+        // install feedback move mouse listener for client-side move updates
+        this.feedbackMoveMouseListener = new FeedbackMoveMouseListener();
+        this.mouseTool.register(this.feedbackMoveMouseListener);
+
+        // instlal change bounds mouse listener for client-side resize updates and server-side updates
+        this.changeBoundsMouseListener = new ChangeBoundsMouseListener();
+        this.mouseTool.register(this.changeBoundsMouseListener);
+        this.commandStack.registerObserver(this.changeBoundsMouseListener);
+    }
+
+    disable() {
+        this.mouseTool.deregister(this.changeBoundsMouseListener);
+        this.mouseTool.deregister(this.feedbackMoveMouseListener);
+    }
+
+}
+
+class ChangeBoundsMouseListener extends MouseListener implements CommandStackObserver {
+    // members for calculating the correct position change
+    private lastDragPosition: Point | undefined = undefined;
+    private positionDelta: Point = { x: 0, y: 0 };
+
+    // members for resize mode
+    private activeResizeElementId: string | undefined = undefined;
+    private activeResizeHandle: SResizeHandle | undefined = undefined;
+
+    mouseDown(target: SModelElement, event: MouseEvent): Action[] {
+        const actions: Action[] = [];
+        if (event.button === 0) {
+            let active: boolean = false;
+            // check if we have a resize handle (only single-selection)
+            if (target instanceof SResizeHandle) {
+                this.activeResizeHandle = target;
+                active = true;
+            } else {
+                // check if we have a moveable element (multi-selection allowed)
+                const moveableElement = findParentByFeature(target, isBoundsAwareMoveable);
+
+                // check if we have a resizeable element (only single-selection)
+                const action = createSwitchResizeModeAction(target);
+                this.activeResizeElementId = action.elementsToActivate.length > 0 ? action.elementsToActivate[0] : undefined;
+                actions.push(action);
+
+                active = moveableElement !== undefined || this.activeResizeElementId !== undefined;
+            }
+            if (active) {
+                this.initPosition(event);
+            } else {
+                this.resetPosition();
+            }
+        }
+        return actions;
+    }
+
+    mouseMove(target: SModelElement, event: MouseEvent): Action[] {
+        if (this.updatePosition(target, event)) {
+            // rely on the FeedbackMoveMouseListener to update the element bounds of selected elements
+            // consider resize handles ourselves
+            return this.handleElementResize();
+        }
+        return [];
+    }
+
+    mouseUp(target: SModelElement, event: MouseEvent): Action[] {
+        if (!this.hasPositionDelta()) {
+            return [];
+        }
+
+        // no further bound changing, simply send the latest data to the server using a single change bounds action for all relevant elements
+        const actions: Action[] = [];
+        if (this.activeResizeHandle) {
+            // An action. Resize, not move.
+            const resizeElement = findParentByFeature(this.activeResizeHandle, isResizeable);
+            if (this.isActiveResizeElement(resizeElement)) {
+                createChangeBoundsAction(resizeElement).forEach(action => actions.push(action));
+            }
+        } else {
+            // Bounds... Change Bounds.
+            const newBounds: ElementAndBounds[] = [];
+            forEachElement(target, isSelectedBoundsAware, element =>
+                createElementAndBounds(element).forEach(bounds => newBounds.push(bounds)));
+            if (newBounds.length > 0) {
+                actions.push(new ChangeBoundsOperationAction(newBounds));
+            }
+        }
+        this.resetPosition();
+        return actions;
+    }
+
+    beforeServerUpdate(model: SModelRoot): void {
+        const isResizeElement = (element: SModelElement): element is SModelElement => this.isActiveResizeElement(element);
+        forEachElement(model, isResizeElement, addResizeHandles);
+    }
+
+    private isActiveResizeElement(element: SModelElement | undefined): element is SParentElement & BoundsAware {
+        return element !== undefined && element.id === this.activeResizeElementId;
+    }
+
+    private initPosition(event: MouseEvent) {
+        this.lastDragPosition = { x: event.pageX, y: event.pageY };
+    }
+
+    private updatePosition(target: SModelElement, event: MouseEvent): boolean {
+        if (this.lastDragPosition) {
+            const viewport = findParentByFeature(target, isViewport);
+            const zoom = viewport ? viewport.zoom : 1;
+            const dx = (event.pageX - this.lastDragPosition.x) / zoom;
+            const dy = (event.pageY - this.lastDragPosition.y) / zoom;
+
+            this.positionDelta = { x: dx, y: dy };
+            this.lastDragPosition = { x: event.pageX, y: event.pageY };
+            return true;
+        }
+        return false;
+    }
+
+    private resetPosition() {
+        this.activeResizeHandle = undefined;
+        this.lastDragPosition = undefined;
+        this.positionDelta = { x: 0, y: 0 };
+    }
+
+    private hasPositionDelta(): boolean {
+        return this.positionDelta.x !== 0 || this.positionDelta.y !== 0;
+    }
+
+    private handleElementResize(): Action[] {
+        if (!this.activeResizeHandle) {
+            return [];
+        }
+
+        const actions: Action[] = [];
+        const resizeElement = findParentByFeature(this.activeResizeHandle, isResizeable);
+        if (this.isActiveResizeElement(resizeElement)) {
+            switch (this.activeResizeHandle.location) {
+                case ResizeHandleLocation.TopLeft:
+                    createSetBoundsAction(resizeElement,
+                        resizeElement.bounds.x + this.positionDelta.x,
+                        resizeElement.bounds.y + this.positionDelta.y,
+                        resizeElement.bounds.width - this.positionDelta.x,
+                        resizeElement.bounds.height - this.positionDelta.y)
+                        .forEach(action => actions.push(action));
+                    break;
+                case ResizeHandleLocation.TopRight:
+                    createSetBoundsAction(resizeElement,
+                        resizeElement.bounds.x,
+                        resizeElement.bounds.y + this.positionDelta.y,
+                        resizeElement.bounds.width + this.positionDelta.x,
+                        resizeElement.bounds.height - this.positionDelta.y)
+                        .forEach(action => actions.push(action));
+                    break;
+                case ResizeHandleLocation.BottomLeft:
+                    createSetBoundsAction(resizeElement,
+                        resizeElement.bounds.x + this.positionDelta.x,
+                        resizeElement.bounds.y,
+                        resizeElement.bounds.width - this.positionDelta.x,
+                        resizeElement.bounds.height + this.positionDelta.y)
+                        .forEach(action => actions.push(action));
+                    break;
+                case ResizeHandleLocation.BottomRight:
+                    createSetBoundsAction(resizeElement,
+                        resizeElement.bounds.x,
+                        resizeElement.bounds.y,
+                        resizeElement.bounds.width + this.positionDelta.x,
+                        resizeElement.bounds.height + this.positionDelta.y)
+                        .forEach(action => actions.push(action));
+                    break;
+            }
+        }
+        return actions;
+    }
+}
+
+function createSwitchResizeModeAction(target: SModelElement): SwitchResizeModeAction {
+    // all elements are deactivated by default
+    const unresizeable: string[] = [];
+    getIndex(target).all().forEach(element => unresizeable.push(element.id));
+
+    // mark single resizeable element active if possible
+    const resizeableElement = findParentByFeature(target, isResizeable);
+    const resizeable: string[] = resizeableElement ? [resizeableElement.id] : [];
+    return new SwitchResizeModeAction(resizeable, unresizeable);
+}
+
+function createChangeBoundsAction(element: SModelElement & BoundsAware): Action[] {
+    if (isValidBoundChange(element, element.bounds)) {
+        return [new ChangeBoundsOperationAction([{ elementId: element.id, newBounds: element.bounds }])];
+    }
+    return [];
+}
+
+function createElementAndBounds(element: SModelElement & BoundsAware): ElementAndBounds[] {
+    if (isValidBoundChange(element, element.bounds)) {
+        return [{ elementId: element.id, newBounds: element.bounds }];
+    }
+    return [];
+}
+
+function createSetBoundsAction(element: SModelElement & BoundsAware, x: number, y: number, width: number, height: number): Action[] {
+    const bounds = { x: x, y: y, width: width, height: height };
+    if (isValidBoundChange(element, bounds)) {
+        return [new SetBoundsAction([{ elementId: element.id, newBounds: bounds }])];
+    }
+    return [];
+}
+
+function isValidBoundChange(element: SModelElement & BoundsAware, bounds: Bounds): boolean {
+    return bounds.width >= minWidth(element) && bounds.height >= minHeight(element);
+}
+
+function minWidth(element: SModelElement & BoundsAware): number {
+    // currently there are no element-specific constraints
+    return 1;
+}
+
+function minHeight(element: SModelElement & BoundsAware): number {
+    // currently there are no element-specific constraints
+    return 1;
+}
