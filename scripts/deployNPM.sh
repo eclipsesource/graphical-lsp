4c25dfecdfc6070fa8b744148054265348aa961e
Tobias Ortmayr <tortmayr@eclipsesource.com>
2019-04-18 11:29:55 +0200

    Fix #235 Fix deployment of next packages

--- scripts/deployNPM.sh
+++ scripts/deployNPM.sh
@@ -3,8 +3,6 @@
 if 	git diff --name-only HEAD^| grep -q "^client" 
 
 then 
-	echo "Set version to next"
-	./setVersions.sh next
 	echo "Deploy next-packages to npm"
 	cd ../client
 	yarn run publish:next
