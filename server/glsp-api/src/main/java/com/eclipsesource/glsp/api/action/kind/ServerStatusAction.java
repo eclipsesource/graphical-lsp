1b7e5b66d41dda67be8a8dc9f3384783125590fd
Tobias Ortmayr <tortmayr@eclipsesource.com>
2019-08-22 16:21:13 +0200

    Add additional constructors for ServerStatusAction

--- server/glsp-api/src/main/java/com/eclipsesource/glsp/api/action/kind/ServerStatusAction.java
+++ server/glsp-api/src/main/java/com/eclipsesource/glsp/api/action/kind/ServerStatusAction.java
@@ -17,6 +17,7 @@ package com.eclipsesource.glsp.api.action.kind;
 
 import com.eclipsesource.glsp.api.action.Action;
 import com.eclipsesource.glsp.api.types.ServerStatus;
+import com.eclipsesource.glsp.api.types.ServerStatus.Severity;
 
 public class ServerStatusAction extends Action {
 	private String severity;
@@ -25,6 +26,14 @@ public class ServerStatusAction extends Action {
 
 	public ServerStatusAction() {
 		super(Action.Kind.SERVER_STATUS);
+	}
+
+	public ServerStatusAction(Severity severity, String message) {
+		this(new ServerStatus(severity, message));
+	}
+
+	public ServerStatusAction(Severity severity, String message, String details) {
+		this(new ServerStatus(severity, message, details));
 	}
 
 	public ServerStatusAction(ServerStatus status) {
